ef80ec0ded70799b2ac30e47d3b16ab7e115ac96
Drago07 <adrien.cauchefer@usherbrooke.ca>
2012-09-12 16:25:07 +0200

    Hello Joel

--- persist-charge/src/main/java/Projet.java
+++ persist-charge/src/main/java/Projet.java
@@ -7,6 +7,7 @@ public class Projet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Adrien");
+		System.out.println("Hello Joel");
 
 	}
 
