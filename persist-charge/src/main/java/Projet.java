b00afae61e98be1a107a036afdbe4b46d80b7eea
jojofr <jsan.fr@gmail.com>
2012-09-13 10:17:57 -0400

    test conflits sur le projet -> projet.java

--- persist-charge/src/main/java/Projet.java
+++ persist-charge/src/main/java/Projet.java
@@ -6,10 +6,9 @@ public class Projet {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Adrien");
-		System.out.println("Hello Joel");
-		System.out.println("Hello JoelADRIEN");
-		System.out.println("Dernier test");
+		
+		
+		System.out.println("Test de conflits");
 		
 
 	}
